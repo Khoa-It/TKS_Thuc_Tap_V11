70f0d1fe12352d2aa84e13ccf6848b5c85441d45
Khoa-It <nguyendangyukhoa01@gmail.com>
2025-08-18 14:35:17 +0700

    fix function point number

--- TKS_Thuc_Tap_V11_Data_Access/Controller/XNK/CXNK_Nhap_Kho_Raw_Data_Controller.cs
+++ TKS_Thuc_Tap_V11_Data_Access/Controller/XNK/CXNK_Nhap_Kho_Raw_Data_Controller.cs
@@ -45,7 +45,7 @@ namespace TKS_Thuc_Tap_V11_Data_Access.Controller.XNK
             return v_arrRes;
         }
 
-        public List<CXNK_Nhap_Kho_Raw_Data> F3001_sp_sel_List_NKRD_For_Form_List(DateTime? p_dtmFrom, DateTime? p_dtmTo)
+        public List<CXNK_Nhap_Kho_Raw_Data> F3001_sp_sel_List_NKRD_For_Form_List_By_Date(DateTime? p_dtmFrom, DateTime? p_dtmTo)
         {
             List<CXNK_Nhap_Kho_Raw_Data> v_arrRes = new List<CXNK_Nhap_Kho_Raw_Data>();
             DataTable v_dt = new DataTable();
@@ -55,7 +55,7 @@ namespace TKS_Thuc_Tap_V11_Data_Access.Controller.XNK
                 p_dtmFrom = CUtility_Date.Convert_To_Dau_Ngay(p_dtmFrom);
                 p_dtmTo = CUtility_Date.Convert_To_Cuoi_Ngay(p_dtmTo);
 
-                CSqlHelper.FillDataTable(CConfig.TKS_Thuc_Tap_V11_Conn_String, v_dt, "F3001_sp_sel_List_NKRD_For_Form_List", p_dtmFrom, p_dtmTo);
+                CSqlHelper.FillDataTable(CConfig.TKS_Thuc_Tap_V11_Conn_String, v_dt, "F3001_sp_sel_List_NKRD_For_Form_List_By_Date", p_dtmFrom, p_dtmTo);
 
                 foreach (DataRow v_row in v_dt.Rows)
                 {
